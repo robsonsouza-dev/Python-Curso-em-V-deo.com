e1fde1e3ba8cc28b424db6acdcb8c81179499f07
Robson <robsonsouza.eng@outlook.com>
2021-08-03 18:09:24 -0300

    commit inicial

---
+++ Desafio 1.py
@@ -0,0 +1,11 @@
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite o segundo número: '))
+n3 = float(input('Digite o terceiro número: '))
+print('')
+s = n1 + n2 + n3
+m = n1 * n2
+d = n2 / n3
+print('')
+print('A soma entre {}, {} e {} é {}' .format(n1, n2, n3, s))
+print('A multiplicação entre {} e {} é {}'. format(n1, n2, m))
+print('A divisão entre {} e {} é {}'. format(n2, n3, d))
